739745b71096c8179ab94fdd7440040594a5c290
Vikrant Singh <vikrant.singh@aapc.com>
2020-09-20 00:00:13 +0530

    commit changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,15 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js*");
     eleventyConfig.addPassthroughCopy("src/css*");
     eleventyConfig.addPassthroughCopy("src/img*");
+
+    eleventyConfig.setTemplateFormats([
+        "css", // css is not yet a recognized template extension in Eleventy
+        "jpg",
+        "gif",
+        "png",
+        "webp"
+      ]);
+
     return{
         dir:{
             input:"src",
