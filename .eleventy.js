ccad112e8977ba502f894bb1b8e6dafe1c7541cb
Vikrant Singh <vikrant.singh@aapc.com>
2020-09-19 23:07:58 +0530

    merge initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+   
+    eleventyConfig.addPassthroughCopy("src/js*");
+    eleventyConfig.addPassthroughCopy("src/css*");
+    eleventyConfig.addPassthroughCopy("src/img*");
+    return{
+        dir:{
+            input:"src",
+            output:"dist"
+        }
+    };
+
+
+  };
