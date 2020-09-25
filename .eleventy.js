ea636222b5f8411862dd2f47d9459a73829d31a4
Vikrant Singh <it.vikrant@gmail.com>
2020-09-25 19:32:05 +0530

    commit changes

--- .eleventy.js
+++ .eleventy.js
@@ -8,9 +8,7 @@ module.exports = function(eleventyConfig) {
     return{
         dir:{
             input:"src",
-            output:"dist",
-            includes: "_includes",
-            layouts: "_layouts"
+            output:"dist"
         }
     };
 
