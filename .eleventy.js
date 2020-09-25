c23c8f59df9d2e2f0c280dcded56edc4a0e5f4bd
Vikrant Singh <it.vikrant@gmail.com>
2020-09-25 18:46:18 +0530

    commit changes

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css*");
     eleventyConfig.addPassthroughCopy("src/img*");
 
-    eleventyConfig.setTemplateFormats([
-      "css", // css is not yet a recognized template extension in Eleventy
-      "jpg",
-      "gif",
-      "png",
-      "webp",
-      "njk"
-    ]);
-
+    
     return{
         dir:{
             input:"src",
